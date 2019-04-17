fa69b7ed4d510e7eed2ef8c65b4e0529c84cd454
Doug Enas <douglassenas@gmail.com>
2019-04-17 12:47:08 -0400

    remove project folder

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 #! python3
 
 from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
+
+project_root = os.path.dirname("./")
+env_path = os.path.join(project_root, ".env")
+load_dotenv(dotenv_path=env_path, verbose=True, override=True)
 
 app = Flask(__name__)
 
