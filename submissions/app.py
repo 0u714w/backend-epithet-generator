577d05fa3f30b5070e86d4c9fcd08cf817852139
Doug Enas <douglassenas@gmail.com>
2019-04-20 19:25:49 -0400

    added flask to pipfile

--- submissions/app.py
+++ submissions/app.py
@@ -5,7 +5,6 @@ from .helpers import Vocabulary, EpithetGenerator
 import os
 
 app = Flask(__name__)
-
 path = os.path.abspath("resources/data.json")
 
 @app.route('/')
@@ -14,15 +13,15 @@ def generate_epithets():
     epithet = d.one_random_word()
     return jsonify(epithet)
 
-@app.route('/vocabulary')
-def generate_vocabulary():
-    v = Vocabulary()
-    vocab = v.read_json(path)
-    return jsonify(vocab["Column 1"] + vocab["Column 2"] + vocab["Column 3"])
+# @app.route('/vocabulary')
+# def generate_vocabulary():
+#     v = Vocabulary()
+#     vocab = v.read_json(path)
+#     return jsonify(vocab["Column 1"] + vocab["Column 2"] + vocab["Column 3"])
 
-@app.route('/<quantity>')
-def generate_epithets_by_quantity(quantity):
-    e = EpithetGenerator()
-    epithets = e.quantity(quantity)
-    return jsonify(epithets)
+# @app.route('/<quantity>')
+# def generate_epithets_by_quantity(quantity):
+#     e = EpithetGenerator()
+#     epithets = e.quantity(quantity)
+#     return jsonify(epithets)
 
